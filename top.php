4630001e38197fdc4ac3c1daa274ac65fa7f08c8
BouleHondevice <boule.hondevice@braldahim.com>
2006-07-30 20:46:14 +0000

    Initial Revision.
    git-svn-id: http://relaismago.googlecode.com/svn/trunk/outils@2 d1a15e38-cf19-0410-8bbc-dd522189590c

---
+++ top.php
@@ -0,0 +1,166 @@
+<?php
+header("Pragma: no-cache");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-cache, must-revalidate");
+
+if (strstr($_SERVER['HTTP_HOST'],"relaismago.cat-the-psion.net")) {
+	die("L'adresse est : <a href='http://outils.relaismago.com'>http://outils.relaismago.com</a>");
+}
+
+require_once('functions_auth.php');
+require_once('functions_display.php');
+require_once('functions_help.php');
+require_once('options_functions_db.php');
+
+require_once('includes/troll.class.php');
+require_once('loteries/loterie.class.php');
+require_once('loteries/loteries.class.php');
+require_once('loteries/loterie_participant.class.php');
+require_once('loteries/loterie_participants.class.php');  
+require_once('loteries/inc_loterie.php');  
+
+require_once('includes/ggc_groupe.class.php');
+require_once('includes/recherche.class.php');
+require_once('includes/options.class.php');
+
+setlocale (LC_TIME, 'fr_FR.ISO8859-1');
+initAuth();
+
+?>
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<!--<script SRC="/menu/ssmMenu.js" language="JavaScript1.2"></script>
+<script src="/menu/ssmItems.php" language="JavaScript1.2"></script>-->
+
+<!-- start -->
+<style>
+all.clsMenuItemNS, .clsMenuItemIE{text-decoration: none; font: bold 12px Arial; color: white; cursor: hand; z-index:1000}
+#MainTable A:hover {color: yellow;}
+</style>
+
+<script language="JavaScript">
+
+//Top Nav Bar I v2.1- By Constantin Kuznetsov Jr.
+//Modified by Dynamic Drive for various improvements
+//Visit http://www.dynamicdrive.com for this script
+
+var keepstatic=0 //specify whether menu should stay static 0=non static (works only in IE4+)
+var menucolor="#000000" //specify menu color
+var menucolorItem="#30385C" //specify menu color
+var menucolorOver="#3038b5" //specify menu color
+var submenuwidth=150 //specify sub menus' color
+
+</script>
+<!-- end -->
+
+
+<link rel='stylesheet' href='/css/MH_Style_Play.css' type='text/css'>
+<link rel='stylesheet' href='/css/feuille2.css' type='text/css'>
+<link rel='stylesheet' href='/css/vue.css' type='text/css'>
+<script type="text/javascript" src="/js/overlib.js"><!-- overLIB (c) Erik Bosrup --></script>
+<script type="text/javascript" src="/js/overlib_exclusive.js"><!-- overLIB (c) Erik Bosrup --></script>
+<script type="text/javascript" src="/js/prototype.js"></script>
+<script type="text/javascript" src="/js/effects.js"></script>
+<script language="javascript" type="text/javascript" src="/js/ajax.js"></script>
+<script language="javascript" type="text/javascript" src="/js/recherche.js"></script>
+<script src="js/livesearch.js"></script>	
+</head>
+<body onload="liveSearchInit()">
+
+<script language="JavaScript" src="/menu/menu.js"></script>
+<script language="JavaScript" src="/menu/ssmItems.php"></script>
+
+<!-- start -->
+<script language="JavaScript">
+showToolbar();
+</script>
+<script language="JavaScript">
+function UpdateIt(){
+if (ie&&keepstatic&&!opr6)
+document.all["MainTable"].style.top = document.body.scrollTop;
+setTimeout("UpdateIt()", 200);
+}
+UpdateIt();
+</script>
+<!-- end -->
+
+<br><br><table width='100%'>
+<tr valign='top'><td>
+<? afficherFormAuth(); ?>
+</td><td align='left' width='60%' valign='top'>
+
+<? if ( userIsGuilde() ) { ?>
+<table width="100%" class='mh_tdborder'>
+	<tr class="mh_tdtitre">
+		<td>
+			<marquee behavior="scroll" align="center" direction="left" scrollamount="2" scrolldelay="3" onmouseover='this.stop()' onmouseout='this.start()'>
+
+Si vous trouvez une anomalie, merci de l'inscrire. <a href='http://trollforge.lipyx.net/bugs/?func=additem&group=relaismago'>ici</a> 
+ &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+Nouveau cockpit en d&eacute;veloppement. La recherche n'est pas compl&eacute;tement fonctionnelle, des choses ne sont pas encore remises... Voir forum pour plus d'informations. Fin pr&eacute;vue dans une / deux semaines.
+ 
+ &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+			</marquee>
+		</td>
+	</tr>
+		<?
+		loterie_info_top();
+		?>
+</table>
+<? } ?>
+
+
+</td>
+
+<? if ( userIsGuilde() ) { 
+	echo "<td align='left' width='40%' valign='top'>";
+	afficherAccesRapide();
+	$recherche = new recherche();
+	echo $recherche->get_html_input();
+	echo "</td>";
+} ?>
+
+</tr>
+</table>
+
+<? 
+unset($_SESSION['options']);
+if ( userIsGuilde() ) { 
+	if (!isset($_SESSION['options'])) {
+		$options = new options($_SESSION['AuthTroll']);
+		$_SESSION["options"] = $options->get_options_tab();
+	}
+	unset($options);
+	$options = $_SESSION["options"];
+
+if ($options["date_option"] == '') {
+	?>
+	<table class='mh_tdborder' align='center' width='80%'>
+		<tr align='center' class=mh_tdborder'>
+			<td colspan='3' class='mh_tdtitre'>
+				<h2><font color='red'>Nouvelles options disponibles et vous ne les avez pas encore
+				r&eacute;gl&eacute;es ! C'est par là => <a href='/options.php'>Ici</a></font></h2>
+			</td>
+		</tr>
+	</table><br>
+
+	<?
+}
+?>
+    <div id="status_recherche" style="display: none">
+      <div class="progressBar" id="progress_bar">
+        <div class="border" id='border'>
+          <div class="background" id='background_recherche'>
+            <div class="foreground" id='foreground_recherche'>Patience...</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="recherche_cage">
+    </div>
+<?
+} 
+?>
