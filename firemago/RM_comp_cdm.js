7b44a964b2d8bce80b19247c5cdc7feb1af585c3
PulgMaltertre <pulg.maltertre@braldahim.com>
2007-06-02 13:19:22 +0000

    recalcul du nb de pv restants
    git-svn-id: http://relaismago.googlecode.com/svn/trunk/outils@72 d1a15e38-cf19-0410-8bbc-dd522189590c

--- firemago/RM_comp_cdm.js
+++ firemago/RM_comp_cdm.js
@@ -102,7 +102,7 @@ function processCdM ()
     { //pas de ligne du type 'Points de Vie : Jamais vu (supérieur à XXX)'
     	var pv1 = parseInt ( pv_line.substring ( pv_line.indexOf ( '(entre ' ) + 7, pv_line.indexOf ( ' et' ) ) );
       var pv2 = parseInt ( pv_line.substring ( pv_line.indexOf ( 'et ' ) + 3, pv_line.indexOf ( ')' ) ) );
-			var p = 1 - b/100;
+			//var p = 1 - b/100;
       /*var pva1 = Math.floor ( pv1 * ( 95 - b ) / 100 );
 	    if ( b == 95 )
 	    {
@@ -110,8 +110,8 @@ function processCdM ()
 	    	b = 101;
 	    }
       var pva2 = Math.floor ( pv2 * ( 105 - b ) / 100 ) -1;*/
-			var pva1 = Math.floor ( pv1 * ( p - 0.05) + 1 );
-			var pva2 = Math.floor ( pv2 * ( p + 0.05));
+			var pva1 = Math.floor ( pv1 * ( 95 - b ) / 100 ) + 1;
+			var pva2 = Math.floor ( pv2 * ( 105 - b ) / 100 );
 	    var vieTd = document.createElement( 'td' );
       vieTd.appendChild ( document.createTextNode ( 'Points de Vie restants (Approximatif) :' ) );
       vieTd.setAttribute ( 'style', 'font-weight:bold;' );
