6d4eae9fecb790e1542c37e0180dc3149178e432
PulgMaltertre <pulg.maltertre@braldahim.com>
2008-10-08 18:10:20 +0000

    quelques améliorations : - affichage des liens - prise en compte des guildes tk ou ennemie
    git-svn-id: http://relaismago.googlecode.com/svn/trunk/outils@171 d1a15e38-cf19-0410-8bbc-dd522189590c

--- firemago/menaces.php
+++ firemago/menaces.php
@@ -36,16 +36,19 @@ if ( userIsLogged () )
 		$html.="<tr class='mh_tdtitre'><td width='25'><b>Dist</b></td><td width='40'><b>Ref.</b></td><td><b>Nom</b></td><td width='25' align='center'><b>X</b></td><td width='25' align='center'><b>Y</b></td><td width='25' align='center'><b>N</b></td></tr>";
 		while ($row = mysql_fetch_array($res))
 		{
-			$html.="<tr class='mh_tdpage'><td>$row[3]</td><td><a href='javascript:EMV($row[4],750,550)'>".$row[4]."</a></td><td>".$row[5]."</td><td align='center'>".$row[0]."</td><td align='center'>".$row[1]."</td><td align='center'>".$row[2]."</td></tr>";
+			$s=preg_split("/[\[\]]/",$row[5]);
+  			$Monstre=htmlspecialchars(trim($s[0]));
+  			$Age=htmlspecialchars(trim($s[1]));
+			$html.="<tr class='mh_tdpage'><td>$row[3]</td><td><a href='javascript:EMV($row[4],750,550)' class='mh_monstres'>".$row[4]."</a></td><td><a href='http://$_SERVER[SERVER_NAME]/bestiaire2/bestiaire.php?Monstre=$Monstre&amp;Age=$Age' class='mh_monstres'>".htmlspecialchars($row[5])."</a></td><td align='center'>".$row[0]."</td><td align='center'>".$row[1]."</td><td align='center'>".$row[2]."</td></tr>";
 		}
 		$html.="</table>";
 	}
 	
 	$sql = "SELECT x_troll, y_troll, z_troll, GREATEST( ABS($x - x_troll), ABS($y - y_troll), ABS($z - z_troll)) as dist,";
-	$sql .= " id_troll, nom_troll, nom_guilde, is_seen_troll, date_format(date_troll,'%d/%m/%Y')";
+	$sql .= " id_troll, nom_troll, nom_guilde, is_seen_troll, date_format(date_troll,'%d/%m/%Y'), id_guilde";
 	$sql .= " FROM trolls, guildes";
 	$sql .= " WHERE (is_tk_troll='oui'";
-	$sql .= " OR is_wanted_troll='oui')";
+	$sql .= " OR is_wanted_troll='oui' OR statut_guilde IN ('tk','ennemie'))";
 	$sql .= " AND GREATEST( ABS($x - x_troll), ABS($y - y_troll), ABS($z - z_troll)) <= $dist_max";
 	$sql .= " AND guilde_troll=id_guilde";
 	$sql .= " ORDER BY dist";
@@ -59,12 +62,22 @@ if ( userIsLogged () )
 		$html.="<tr class='mh_tdtitre'><td width='25'><b>Dist</b></td><td width='40'><b>Ref.</b></td><td><b>Nom</b></td><td><b>Guilde</b></td><td width='25' align='center'><b>X</b></td><td width='25' align='center'><b>Y</b></td><td width='25' align='center'><b>N</b></td></tr>";
 		while ($row = mysql_fetch_array($res))
 		{
-			$html.="<tr class='mh_tdpage'><td>$row[3]</td><td><a href='javascript:EPV($row[4],750,550)'>".$row[4]."</a></td><td>".htmlspecialchars($row[5]);
+			$html.="<tr class='mh_tdpage'><td>$row[3]</td><td><a href='javascript:EPV($row[4],750,550)' class='mh_trolls_1'>".$row[4]."</a></td><td><a href='http://$_SERVER[SERVER_NAME]/engine_view.php?troll=$row[4]' class='mh_trolls_1'>".htmlspecialchars($row[5])."</a>";
 			if ( $row[7] == 'non' )
 			{
-				$html.="<img height='20' src='http://$_SERVER[SERVER_NAME]/images/fantome-fixe.png' title='pas vu depuis le $row[8]'/>";
+				$html.="<img height='20' src='http://$_SERVER[SERVER_NAME]/images/fantome-fixe.png' title='disparu le $row[8]'/>";
 			}
-			$html.="</td><td>".htmlspecialchars($row[6])."</td><td align='center'>".$row[0]."</td><td align='center'>".$row[1]."</td><td align='center'>".$row[2]."</td></tr>";
+			$html.="</td><td>";
+			if ( $row[9] != 1)
+			{
+				$html.="[<a href='http://$_SERVER[SERVER_NAME]/engine_view.php?guilde=$row[9]' class='mh_links'>RG</a>] - ";
+				$html.="<a href='javascript:EAV($row[9],750,550)' class='mh_links'>".htmlspecialchars($row[6])."</a>";
+			}
+			else
+			{
+				$html.=htmlspecialchars($row[6]);
+			}
+			$html.="</td><td align='center'>".$row[0]."</td><td align='center'>".$row[1]."</td><td align='center'>".$row[2]."</td></tr>";
 		}
 		$html.="</table>";
 	}
