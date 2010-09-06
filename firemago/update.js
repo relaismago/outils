231bf52bc4bdd9dbed25cc6773619df59b4c2452
PulgMaltertre <pulg.maltertre@braldahim.com>
2010-09-06 21:05:26 +0000

    compatibilité firefox 3.6
    git-svn-id: http://relaismago.googlecode.com/svn/trunk/outils@199 d1a15e38-cf19-0410-8bbc-dd522189590c

--- firemago/update.js
+++ firemago/update.js
@@ -1,2 +1,2 @@
-if(numVersionFiremago != "2.3")
+if(numVersionFiremago != "2.4")
       alert("Une nouvelle version de Firemago est disponible\n\nVeuillez mettre l'extension à jour en cliquant dans outils puis modules complémentaires puis mise à jour.\n(vous pouvez désactiver ce messages dans la page de configuration de firemago)");
